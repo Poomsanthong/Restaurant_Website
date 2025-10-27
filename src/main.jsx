2e209f1060482f4aec234a9d0271834d0f277b3c
Poomsanthong <thanapoom.santhong10@gmail.com>
2025-10-27 21:46:46 +0100

    Add @vercel/analytics dependency and integrate Analytics component in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App.jsx";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { Analytics } from "@vercel/analytics/next";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
